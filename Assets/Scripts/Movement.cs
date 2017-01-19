da4a07238ce2842bc2350ee79301c31bad68a86d
Spencer Pollock <srepollock@gmail.com>
2017-01-19 10:42:39 -0800

    :beer: Finished 100%

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,13 +2,23 @@
 
 public class Movement : MonoBehaviour {
 
+	public Car c;
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KeyCode.W)) {
+			c.transform.position += new Vector3(0f, 0f, 10f);
+		} else if (Input.GetKeyDown(KeyCode.A)) {
+			c.transform.position += new Vector3(-10f, 0f, 0f);
+		} else if (Input.GetKeyDown(KeyCode.S)) {
+			c.transform.position += new Vector3(0f, 0f, -10f);
+		} else if (Input.GetKeyDown(KeyCode.D)) {
+			c.transform.position += new Vector3(10f, 0f, 0f);
+		}
 	}
 }

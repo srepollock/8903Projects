d2cf25ad3ca89c328facb9d9941a11ebea3ac8d4
Spencer Pollock <srepollock@gmail.com>
2017-01-24 15:08:32 -0800

    :beer: Minor work. Just want to save before I head home

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Projectile : MonoBehaviour {
+	/// <summary>
+	/// Reference to the weapon shooting the projectile.
+	/// </summary>
+	public WeaponRotation gun;
     public float initialVelocity = 100f; // 100 m/(s^2)
 	/// <summary>
     /// Current Speed
@@ -10,6 +15,7 @@ public class Projectile : MonoBehaviour {
     /// Constant acceleration;
     /// </summary>
 	public float acceleration = -10f;
+    public Text projectilex, projectiley, velocityText, timeText;
     /// <summary>
     /// Drag or wind resistance on the object.
     /// </summary>
@@ -19,6 +25,8 @@ public class Projectile : MonoBehaviour {
     /// </summary>
 	bool stopped = true;
 
+	float timer = 0;
+
     void Start()
     {
 
@@ -26,25 +34,65 @@ public class Projectile : MonoBehaviour {
 
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.L)) 
+        {
+			drag = !drag;
+		}
+		if (Input.GetKeyDown(KeyCode.Backspace)
+		||	Input.GetKeyDown(KeyCode.R)) 
+        {
+			resetPosition();
+		}
+		if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            // Shoot
+			stopped = !stopped;
+		}
     }
 
     void FixedUpdate()
     {
         if (!stopped) {
 			StartRunning();
+			timer += Time.deltaTime;
 			if (drag) {
 				acceleration = -(0.001f) * Mathf.Pow(speed, 2);
 			}
 			speed += acceleration * Time.deltaTime;
+			//c.transform.position += new Vector3(speed, 0f, 0f) * Time.deltaTime;
 		}
+		UpdateTimeText(timer);
+		UpdateVelocityText(speed);
+		if (timer >= 8) { stopped = true; } // TODO Stop when hitting the target
     }
 
     void StartRunning()
     {
-        if (!stopped)
+        if(timer == 0) 
         {
-            speed = initialVelocity;
-        }
+			speed = initialVelocity;
+		}
     }
+
+    void UpdateTimeText (float t) 
+    {
+		timeText.text = t + " seconds";
+	}
+
+	void UpdateVelocityText (float v) 
+    {
+		velocityText.text = v + " m/s^2";
+	}
+
+	void resetPosition()
+	{
+		this.transform.position = gun.transform.position;
+		timer = 0;
+		stopped = false;
+	}
+
+	public void rotate(Quaternion q)
+	{
+		this.transform.rotation = q;
+	}
 }
---
+++ Assets/Scripts/WeaponRotation.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponRotation : MonoBehaviour {
+
+	public Projectile projectile;
+	public Text rangex, rangey, correctGunAngle, currentGunAngle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+		{
+			// Rotate left
+			this.transform.Rotate(new Vector3(0f, -0.1f, 0f));
+			projectile.rotate(this.transform.rotation);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			// Rotate right
+			this.transform.Rotate(new Vector3(0f, 0.1f, 0f));
+			projectile.rotate(this.transform.rotation);
+		}
+		updateAngle();
+		updateProjectile();
+	}
+
+	void updateAngle()
+	{
+		currentGunAngle.text = Vector3.Angle(Vector3.forward, 
+			this.transform.forward).ToString();
+	}
+
+	void updateProjectile()
+	{
+
+	}
+}

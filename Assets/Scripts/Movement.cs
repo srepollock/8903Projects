ffc959f5020378e739ae0d7017c428117e7c4988
Spencer Pollock <srepollock@gmail.com>
2017-02-18 20:04:15 -0800

    :scream: Setup the view of the scene. Dont know what else to do from here. Definately going to need some help on this one

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,11 +12,11 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.W)) {
-			c.transform.position += new Vector3(0f, 0f, 10f);
+			c.transform.position += new Vector3(0f, 10f, 0f);
 		} else if (Input.GetKeyDown(KeyCode.A)) {
 			c.transform.position += new Vector3(-10f, 0f, 0f);
 		} else if (Input.GetKeyDown(KeyCode.S)) {
-			c.transform.position += new Vector3(0f, 0f, -10f);
+			c.transform.position += new Vector3(0f, -10f, 0f);
 		} else if (Input.GetKeyDown(KeyCode.D)) {
 			c.transform.position += new Vector3(10f, 0f, 0f);
 		}
